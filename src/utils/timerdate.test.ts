84bfc7da152e81a3458394a4b91247363d1d0c89
Kris Scott <kscott91@gmail.com>
2019-09-01 21:15:28 -0500

    Add custom date string formats to timerdate.ts

--- src/utils/timerdate.test.ts
+++ src/utils/timerdate.test.ts
@@ -20,4 +20,46 @@ test('parseDateString()', () => {
     expect(parseDateString('9/12 8pm', ref).getTime()).toBe(
         time('2019-09-12 8:00 pm')
     )
+    expect(parseDateString('9:30pm', ref).getTime()).toBe(
+        time('2019-09-01 9:30 pm')
+    )
+    expect(parseDateString('10:05:56 PM', ref).getTime()).toBe(
+        time('2019-09-01 10:05:56 pm')
+    )
+    expect(parseDateString('12PM', ref).getTime()).toBe(
+        time('2019-09-01 12:00 pm')
+    )
+    expect(parseDateString('23:03', ref).getTime()).toBe(
+        time('2019-09-01 11:03 pm')
+    )
+    expect(parseDateString('1/31/2020 12am', ref).getTime()).toBe(
+        time('2020-01-31 12:00 am')
+    )
+    expect(parseDateString('2/29/2020 6:53:04 pm', ref).getTime()).toBe(
+        time('2020-02-29 6:53:04 pm')
+    )
+
+    // Relative date format
+
+    expect(parseDateString('20s', ref).getTime()).toBe(
+        time('2019-09-01 7:00:20 pm')
+    )
+    expect(parseDateString('20m', ref).getTime()).toBe(
+        time('2019-09-01 7:20 pm')
+    )
+    expect(parseDateString('1h', ref).getTime()).toBe(
+        time('2019-09-01 8:00 pm')
+    )
+    expect(parseDateString('1d', ref).getTime()).toBe(
+        time('2019-09-02 7:00 pm')
+    )
+    expect(parseDateString('1w', ref).getTime()).toBe(
+        time('2019-09-08 7:00 pm')
+    )
+    expect(parseDateString('1y', ref).getTime()).toBe(
+        time('2020-09-01 7:00 pm')
+    )
+    expect(parseDateString('1y 2w 3d 4h 5m 6s', ref).getTime()).toBe(
+        time('2020-09-18 11:05:06 pm')
+    )
 })
--- src/utils/timerdate.ts
+++ src/utils/timerdate.ts
@@ -49,47 +49,147 @@ export const parseDateString = (
     input: string,
     reference: Date = new Date()
 ) => {
+    console.log('==============')
+    console.log(input)
+
     // If input is valid ISO date, parse directly
     const isoDate = new Date(input)
     if (!isNaN(isoDate.getTime())) {
         return isoDate
     }
 
+    // Check for common date format (e.g. '9/2/2019 6:54:02 pm')
     const absDateMatch = input.match(
-        /(\d{1,2})[/-](\d{1,2})\s+(\d{1,2})\s*([ap])m?/
+        /^\s*(?:(\d{1,2})[/-](\d{1,2})(?:[/-](\d{4}))?\s+)?(\d{1,2})(?::(\d{2})(?::(\d{2}))?)?\s*(?:([ap])m?)?\s*$/i
     )
 
     if (absDateMatch) {
-        console.log(input)
         console.log(absDateMatch)
 
-        const [ , monthStr, dayStr, hourStr, ampm ] = absDateMatch
+        const [
+            ,
+            monthStr,
+            dayStr,
+            yearStr,
+            hourStr,
+            minStr,
+            secStr,
+            ampm,
+        ] = absDateMatch
 
-        const month = parseInt(monthStr)
-        const day = parseInt(dayStr)
-        const hour = parseInt(hourStr)
+        const [ month, day, year, hour, min, sec ] = [
+            monthStr,
+            dayStr,
+            yearStr,
+            hourStr,
+            minStr,
+            secStr,
+        ].map((s) => parseInt(s))
 
-        console.log({ month, day, hour, ampm })
+        console.log({
+            monthStr,
+            dayStr,
+            yearStr,
+            hourStr,
+            minStr,
+            secStr,
+            ampm,
+        })
+        console.log({ month, day, year, hour, min, sec, ampm })
 
-        const out = new Date(reference.getTime())
-        if (!isNaN(month)) {
-            out.setMonth(month - 1)
-        }
-        if (!isNaN(day)) {
-            out.setDate(day)
-        }
-        if (!isNaN(hour)) {
-            if (ampm === 'a') {
-                out.setHours(hour === 12 ? 0 : hour)
-            } else if (ampm === 'p') {
-                out.setHours(hour + 12)
-            } else {
-                out.setHours(hour)
+        if (day !== undefined || hour != undefined) {
+            const out = new Date(reference.getTime())
+
+            if (!isNaN(year)) {
+                out.setFullYear(year)
+            }
+            if (!isNaN(month)) {
+                out.setMonth(month - 1)
+            }
+            if (!isNaN(day)) {
+                out.setDate(day)
+            }
+            if (!isNaN(hour)) {
+                if (ampm === 'a' || ampm === 'A') {
+                    out.setHours(hour === 12 ? 0 : hour)
+                } else if (ampm === 'p' || ampm === 'P') {
+                    out.setHours(hour === 12 ? 12 : hour + 12)
+                } else {
+                    out.setHours(hour)
+                }
+            }
+            if (!isNaN(min)) {
+                out.setMinutes(min)
+            }
+            if (!isNaN(sec)) {
+                out.setSeconds(sec)
+            }
+
+            console.log(out.toUTCString())
+
+            if (!isNaN(out.getTime())) {
+                return out
             }
         }
+    }
 
-        console.log(out.toUTCString())
-        return out
+    // Check for relative date format (e.g. '1y 5w 2d 3h 45m 20s')
+    const relDateMatch = input.match(
+        /^\s*(?:(-?\d+)y\s*)?(?:(-?\d+)w\s*)?(?:(-?\d+)d\s*)?(?:(-?\d+)h\s*)?(?:(-?\d+)m\s*)?(?:(-?\d+)s\s*)?$/i
+    )
+
+    if (relDateMatch) {
+        console.log(relDateMatch)
+
+        const [
+            ,
+            yearsStr,
+            weeksStr,
+            daysStr,
+            hoursStr,
+            minsStr,
+            secsStr,
+        ] = relDateMatch
+
+        const [ years, weeks, days, hours, mins, secs ] = [
+            yearsStr,
+            weeksStr,
+            daysStr,
+            hoursStr,
+            minsStr,
+            secsStr,
+        ].map((s) => parseInt(s))
+
+        console.log({ years, weeks, days, hours, mins, secs })
+
+        if ([ years, weeks, days, hours, mins, secs ].some((x) => !isNaN(x))) {
+            const out = new Date(reference.getTime())
+
+            if (!isNaN(years)) {
+                out.setFullYear(out.getFullYear() + years)
+            }
+            if (!isNaN(weeks)) {
+                out.setDate(out.getDate() + weeks * 7)
+            }
+            if (!isNaN(days)) {
+                out.setDate(out.getDate() + days)
+            }
+            if (!isNaN(hours)) {
+                out.setHours(out.getHours() + hours)
+            }
+            if (!isNaN(mins)) {
+                out.setMinutes(out.getMinutes() + mins)
+            }
+            if (!isNaN(secs)) {
+                out.setSeconds(out.getSeconds() + secs)
+            }
+
+            console.log(out.toUTCString())
+
+            if (!isNaN(out.getTime())) {
+                return out
+            }
+        }
     }
 
     // Return invalid date
